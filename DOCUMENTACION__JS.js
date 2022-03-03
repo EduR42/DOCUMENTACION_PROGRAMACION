852f7874b1c54b45688795597c1b50d668e6900f
EduR42 <sistemasedur@gmail.com>
2022-03-03 09:57:49 -0600

    CAMBIOS

--- DOCUMENTACION__JS.js
+++ DOCUMENTACION__JS.js
@@ -33,8 +33,20 @@
  // #endregion 
 
 
- // #region ----->>> BORRA SOLO LOS ESPACIOS EN BLANCO EN UNA CADENA DE TEXTO <<<--------
+// #region ----->>> BORRA SOLO LOS ESPACIOS EN BLANCO EN UNA CADENA DE TEXTO <<<--------
  const str = "DelftS    tacks  ";
  const str2 = str.replace(/\s+/g, '');
  console.log(str2);
 // #endregion 
+
+ // #region ----->>> REEMPLAZA LOS CARACTERES DE UNA CADENA DE TEXTO <<<--------
+const p = 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?';
+console.log(p.replace('dog', 'monkey'));
+// expected output: "The quick brown fox jumps over the lazy monkey. If the dog reacted, was it really lazy?"
+ // #region ----->>> REEMPLAZA LOS CARACTERES DE UNA CADENA DE TEXTO <<<--------
+
+// #region ----->>> PRIMERAS LETRAS DE LAS PALABRAS EN MAYUSCULAS <<<--------
+  const str = "este es mi texto solo en minusculas";
+  str = str.replace(/\b\w/g, l => l.toUpperCase())
+  console.log(str);
+ // #endregion 
